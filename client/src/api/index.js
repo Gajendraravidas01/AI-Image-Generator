4729aab1e7c57c453a5d7e5b3272d013aa1fa463
Gajendra kumar ravidas <ravidasgajendra1425@gmail.com>
2025-05-29 20:17:09 +0530

    changes the baseurl

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // http://localhost:8080/api/
 const API = axios.create({
-  // baseURL: "https://ai-image-generator-eta-gray.vercel.app/api/",
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://ai-image-generator-eta-gray.vercel.app/api/",
+  // baseURL: "http://localhost:8080/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
